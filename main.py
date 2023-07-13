63abf026dbd39e0e7d83c3da44c40956b77de191
Vitor Curtarelli <vitor.curtarelli@gmail.com>
2023-07-13 12:04:13 -0300

    Finished comenting main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import copy
 import os
 
 import numpy as np
@@ -6,19 +5,13 @@ import scipy.linalg
 from numpy.linalg import inv
 from functions import *
 import scipy.special as spsp
-import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
-from mpl_toolkits.axes_grid1 import make_axes_locatable
-import tikzplotlib as tpl
 import scipy as sp
 
 
-def simulation(result: str, sizes: Params, name: str):
+def simulation(sizes: Params, name: str):
     """
     Parameters
     ----------
-    result: str
-        Which result to produce, from the available.
     sizes: Params
         Struct of names and sizes of the desired sub-array beamformers.
     name: str
@@ -38,8 +31,7 @@ def simulation(result: str, sizes: Params, name: str):
         'wng': 'Lineplot - White noise gain (real)',
         'df': 'Lineplot - Directivity factor (real)',
         'snr': 'Lineplot - Signal-to-noise ratio (real)',
-        'polar': "Polar plot - Beampatterns for all BF's and ",
-        'polar2': 'Polar plot - Beampattern for a given frequency',
+        'polar': "Polar plot - Beampatterns for all beamformers",
         'gendata': "Multiplot - Generates 'beam' and 'polar' for the final beamformer"
     }
 
@@ -50,7 +42,7 @@ def simulation(result: str, sizes: Params, name: str):
     sizes.CS = add_arrays(sizes.ss, sizes.cb)
     sizes.PA = add_arrays(sizes.LC, sizes.CS)
 
-    Arr_PA = Array(sizes.PA, "PA")
+    Arr_FA = Array(sizes.PA, "PA")
     Arr_LC = Array(sizes.LC, "LC")
     Arr_CS = Array(sizes.CS, "CS")
     Arr_sd = Array(sizes.sd, "sd")
@@ -59,7 +51,7 @@ def simulation(result: str, sizes: Params, name: str):
     Arr_cb = Array(sizes.cb, "cb")
 
     Arrs = [
-        Arr_PA,
+        Arr_FA,
         Arr_LC,
         Arr_CS,
         Arr_sd,
@@ -146,7 +138,7 @@ def simulation(result: str, sizes: Params, name: str):
     ## ---------------
     ## Sub-array matrices
     for Arr in Arrs:
-        Arr.calc_vals(params, Arr_PA)  # Calculates position for all sensors of sub-array
+        Arr.calc_vals(params, Arr_FA)  # Calculates position for all sensors of sub-array
         Arr.init_metrics(sym_freqs.size, sym_angles.size, Ni)  # Initializes metrics
 
     ## ---------------
@@ -236,234 +228,93 @@ def simulation(result: str, sizes: Params, name: str):
 
         for Arr in Arrs:
             Arr.calc_sv(td_rad, f, c, True)
-
+            
+        # ---------------
+        # Joins sub-array beamformers
         Arr_ss.h = conv_beam(Arr_sd, Arr_ds)
         Arr_CS.h = conv_beam(Arr_ss, Arr_cb)
-        Arr_PA.h = conv_beam(Arr_LC, Arr_CS)
+        Arr_FA.h = conv_beam(Arr_LC, Arr_CS)
 
         for Arr in Arrs:
             Arr.calc_gain()
 
-        # Output metrics
+        ## ---------------
+        ## Output metrics
         for arr_idx, Arr in enumerate(Arrs):
-            if result in ('gendata', 'beam', 'polar', 'polar2', 'fnbw'):
-                # Beampattern
-                for a_idx, angle in enumerate(sym_angles):
-                    angle = np.deg2rad(angle)
-                    d_ta = Arr.calc_sv(angle, f, c, False)
-                    Arr.beam[f_idx, a_idx] = calcbeam(Arr.h, d_ta)
+            for a_idx, angle in enumerate(sym_angles):
+                angle = np.deg2rad(angle)
+                d_ta = Arr.calc_sv(angle, f, c, False)
+                Arr.beam[f_idx, a_idx] = calcbeam(Arr.h, d_ta)
 
-            if result in ('gendata', 'dsdi'):
-                # DSDI
-                Arr.dsdi[f_idx] = calcdsdi(Arr.h, Arr.d_td)
+            Arr.dsdi[f_idx] = calcdsdi(Arr.h, Arr.d_td)
+            Arr.wng[f_idx] = calcwng(Arr.h, Arr.d_td)
+            Arr.df[f_idx] = calcdf(Arr, f, c)
+            Arr.snr[f_idx] = calcsnr(Arr, Var_X, tis_rad, Var_Vis, Var_Vd, Var_Va, f, c)
+            idx_td = np.where(sym_angles == td_deg)[0][0]
+            Arr.fnbw[f_idx] = calcfnbw(Arr.beam[f_idx, :], idx_td, sym_angles)
 
-            if result in ('gendata', 'wng'):
-                # WNG
-                Arr.wng[f_idx] = calcwng(Arr.h, Arr.d_td)
+            bpt = calcbpt(Arr.h, Arr.d_td)
+            bpt = [bpt]
+            for ti_rad in tis_rad:
+                d_ti = Arr.calc_sv(ti_rad, f, c, False)
+                bpt.append(calcbpt(Arr.h, d_ti))
+            Arr.bpt[f_idx, :] = np.array(bpt)
 
-            if result in ('gendata', 'df'):
-                # DF
-                df = calcdf(Arr, f, c)
-                Arr.df[f_idx] = df
+            Arrs[arr_idx] = Arr
 
-            if result in ('gendata', 'snr'):
-                # SNR
-                Arr.snr[f_idx] = calcsnr(Arr, Var_X, tis_rad, Var_Vis, Var_Vd, Var_Va, f, c)
+    ## ---------------
+    ## Data generation/presentation
+        for Arr in Arrs:
+            freqs = sym_freqs.reshape(-1, ) / 1000
+            angles = sym_angles.reshape(-1, )
+            wng = dB(Arr.wng.reshape(-1, ))
+            df = dB(Arr.df.reshape(-1, ))
+            snr = dB(Arr.snr.reshape(-1, ))
+            beam = dB(vect(Arr.beam).reshape(-1, ))
+            fnbw = Arr.fnbw.reshape(-1, )
+            params = [freqs, angles, wng, df, beam, snr, fnbw]
+            params = fixDec(params)
+            params = list(params)
+            for idx, param in enumerate(params):
+                params[idx] = list(param)
 
-            if result in ('gendata', 'fnbw'):
-                # FNBW
-                idx_td = np.where(sym_angles == td_deg)[0][0]
-                Arr.fnbw[f_idx] = calcfnbw(Arr.beam[f_idx, :], idx_td, sym_angles)
+            freqs, angles, wng, df, beam, snr, fnbw = tuple(params)
 
-            if result in ('gendata', 'bpt'):
-                # BPT
-                bpt = calcbpt(Arr.h, Arr.d_td)
-                bpt = [bpt]
-                for ti_rad in tis_rad:
-                    d_ti = Arr.calc_sv(ti_rad, f, c, False)
-                    bpt.append(calcbpt(Arr.h, d_ti))
-                Arr.bpt[f_idx, :] = np.array(bpt)
-                Arrs[arr_idx] = Arr
+            wng_ = list(zip(freqs, wng))
+            df_ = list(zip(freqs, df))
+            snr_ = list(zip(freqs, snr))
+            fnbw_ = list(zip(freqs, fnbw))
+            b_freqs = freqs * len(angles)
+            b_angles = []
+            for angle in angles:
+                b_angles += [angle] * len(freqs)
+            beam_ = list(zip(b_freqs, b_angles, beam))
 
-    XY_f, XY_a = np.meshgrid(sym_angles, sym_freqs)
+            wng_ = 'freq,val\n' + '\n'.join([','.join([str(val) for val in item]) for item in wng_])
+            df_ = 'freq,val\n' + '\n'.join([','.join([str(val) for val in item]) for item in df_])
+            beam_ = 'freq,ang,val\n' + '\n'.join([','.join([str(val) for val in item]) for item in beam_])
+            snr_ = 'freq,val\n' + '\n'.join([','.join([str(val) for val in item]) for item in snr_])
+            fnbw_ = 'freq,val\n' + '\n'.join([','.join([str(val) for val in item]) for item in fnbw_])
 
-    match result:
-        case 'beam':
-            # fix, ax = plt.subplots(2, 2)
-            fig = plt.figure(figsize=(8, 6))
-            gs = gridspec.GridSpec(5, 5)
-            ax1 = plt.subplot(gs[0:2, 0:2])
-            ax2 = plt.subplot(gs[0:2, 3:5])
-            ax3 = plt.subplot(gs[3:5, 1:4])
-
-            axs = [ax1, ax2, ax3]
-            beams = [Arr_LC.beam, Arr_CS.beam, Arr_PA.beam]
-            xticks = [a0 + i * 45 for i in range(int(1 + (a1 - a0) // 45))]
-            yticks = [f0, (f0 + f1) / 2, f1]
-
-            for idx in range(len(axs)):
-                ax = axs[idx]
-                beam = beams[idx]
-                dB_beam = dB(beam)
-                c = ax.pcolormesh(XY_f, XY_a, dB_beam, cmap='viridis', vmin=-50)
-                ax.set_xticks(ticks=xticks)
-                ax.set_yticks(ticks=yticks, labels=['{:.1f}k'.format(ytick / 1000) for ytick in yticks])
-                divider = make_axes_locatable(ax)
-                cax = divider.append_axes('right', size='5%', pad=0.05)
-                fig.colorbar(c, cax=cax, orientation='vertical')
-                ax.axvline(x=td_deg, c='y', linestyle=':')
-                ax.axvline(x=d_tB[0], c='b', linestyle=':')
-                ax.axvline(x=d_tB[1], c='b', linestyle=':')
-                ax.set_xlabel('Angle (º)')
-                ax.set_ylabel('Frequency (Hz)')
-                c.set_clim(-60, 0)
-            plt.show()
-
-        case 'wng':
-            fig, ax = plt.subplots()
-            for arr_idx, Arr in enumerate(Arrs):
-                wng_dB = dB(Arr.wng)
-                label = Arr.name
-                ax.plot(sym_freqs, wng_dB, label=label, linewidth=10 - arr_idx, linestyle=(0, (3, arr_idx)))
-            plt.xlim(f0, f1)
-            # plt.ylim([0, 20])
-            plt.legend()
-            plt.show()
-
-        case 'df':
-            fig, ax = plt.subplots()
-            for arr_idx, Arr in enumerate(Arrs):
-                df_plot = dB(Arr.df)
-                # df_plot = Arr.df
-                label = Arr.name
-                ax.plot(sym_freqs, df_plot, label=label, linewidth=10 - arr_idx, linestyle=(0, (3, arr_idx)))
-            plt.xlim(f0, f1)
-            # plt.ylim([0, 20])
-            plt.legend()
-            plt.show()
-
-        case 'snr':
-            fig, ax = plt.subplots()
-            for arr_idx, Arr in enumerate(Arrs):
-                snr_plot = dB(Arr.snr)
-                # df_plot = Arr.df
-                label = Arr.name
-                ax.plot(sym_freqs, snr_plot, label=label, linewidth=10 - arr_idx, linestyle=(0, (3, arr_idx)))
-            plt.xlim(f0, f1)
-            # plt.ylim([0, 20])
-            plt.legend()
-            plt.show()
-
-        case 'fnbw':
-            fig, ax = plt.subplots()
-            for arr_idx, Arr in enumerate(Arrs):
-                fnbw_plot = Arr.fnbw
-                # df_plot = Arr.df
-                label = Arr.name
-                ax.plot(sym_freqs, fnbw_plot, label=label, linewidth=10 - arr_idx, linestyle=(0, (3, arr_idx)))
-            plt.xlim(f0, f1)
-            plt.ylim([0, 50])
-            plt.legend()
-            plt.show()
-            # plt.plot(sym_angles, Arrs[0].beam[-1, :])
-            # plt.show()
-
-        case 'polar':
-            fig, axs = plt.subplots(2, 4, subplot_kw={'projection': 'polar'})
-            fs = [0, -1]
-            beams = [Arr_LC.beam, Arr_CS.beam, Arr_PA.beam, Arr_LC.beam * Arr_CS.beam]
-            for f_i in range(len(fs)):
-                for b_i in range(len(beams)):
-                    f_i_ = fs[f_i]
-                    f = sym_freqs[f_i_]
-                    pbeam = beams[b_i]
-                    beam_f = dB(pbeam[f_i_, :])
-                    ax = axs[f_i, b_i]
-                    ax.plot(np.deg2rad(sym_angles), beam_f)
-                    ax.set_ylim([-61, 0])
-                    ax.axvline(x=td_rad, c='r')
-                    for i in range(Ni):
-                        ti_rad = tis_rad[i]
-                        ax.axvline(x=ti_rad, c='k', linestyle='--')
-                    ax.axvline(x=td_rad - tB_rad/2, c='k', linestyle=':')
-                    ax.axvline(x=td_rad + tB_rad/2, c='k', linestyle=':')
-                    axs[f_i, b_i] = ax
-            plt.show()
-
-        case 'polar2':
-            fig, axs = plt.subplots(2, 4, subplot_kw={'projection': 'polar'})
-            fs = [0, -1]
-            beams = [Arr_LC.beam, Arr_CS.beam, Arr_ds.beam, Arr_PA.beam]
-            for f_i in range(len(fs)):
-                for b_i in range(len(beams)):
-                    f_i_ = fs[f_i]
-                    f = sym_freqs[f_i_]
-                    pbeam = beams[b_i]
-                    beam_f = dB(pbeam[f_i_, :])
-                    ax = axs[f_i, b_i]
-                    ax.plot(np.deg2rad(sym_angles), beam_f)
-                    ax.set_ylim([-61, 0])
-                    ax.axvline(x=td_rad, c='r')
-                    for i in range(Ni):
-                        ti_rad = tis_rad[i]
-                        ax.axvline(x=ti_rad, c='k', linestyle='--')
-                    ax.axvline(x=td_rad - tB_rad/2, c='k', linestyle=':')
-                    ax.axvline(x=td_rad + tB_rad/2, c='k', linestyle=':')
-                    axs[f_i, b_i] = ax
-            plt.show()
-
-        case 'gendata':
-            for Arr in Arrs:
-                freqs = sym_freqs.reshape(-1, ) / 1000
-                angles = sym_angles.reshape(-1, )
-                wng = dB(Arr.wng.reshape(-1, ))
-                df = dB(Arr.df.reshape(-1, ))
-                snr = dB(Arr.snr.reshape(-1, ))
-                beam = dB(vect(Arr.beam).reshape(-1, ))
-                fnbw = Arr.fnbw.reshape(-1, )
-                params = [freqs, angles, wng, df, beam, snr, fnbw]
-                params = fixDec(params)
-                params = list(params)
-                for idx, param in enumerate(params):
-                    params[idx] = list(param)
-
-                freqs, angles, wng, df, beam, snr, fnbw = tuple(params)
-
-                wng_ = list(zip(freqs, wng))
-                df_ = list(zip(freqs, df))
-                snr_ = list(zip(freqs, snr))
-                fnbw_ = list(zip(freqs, fnbw))
-                b_freqs = freqs * len(angles)
-                b_angles = []
-                for angle in angles:
-                    b_angles += [angle] * len(freqs)
-                beam_ = list(zip(b_freqs, b_angles, beam))
-
-                wng_ = 'freq,val\n' + '\n'.join([','.join([str(val) for val in item]) for item in wng_])
-                df_ = 'freq,val\n' + '\n'.join([','.join([str(val) for val in item]) for item in df_])
-                beam_ = 'freq,ang,val\n' + '\n'.join([','.join([str(val) for val in item]) for item in beam_])
-                snr_ = 'freq,val\n' + '\n'.join([','.join([str(val) for val in item]) for item in snr_])
-                fnbw_ = 'freq,val\n' + '\n'.join([','.join([str(val) for val in item]) for item in fnbw_])
-
-                filename = 'res' + name + '_' + Arr.name + '_'
-                folder = 'results' + name + '/'
-                if not os.path.isdir(folder):
-                    os.mkdir(folder)
-                with open(folder + filename + 'wng.csv', 'w') as f:
-                    f.write(wng_)
-                    f.close()
-                with open(folder + filename + 'df.csv', 'w') as f:
-                    f.write(df_)
-                    f.close()
-                with open(folder + filename + 'snr.csv', 'w') as f:
-                    f.write(snr_)
-                    f.close()
-                with open(folder + filename + 'fnbw.csv', 'w') as f:
-                    f.write(fnbw_)
-                    f.close()
-                with open(folder + filename + 'beam.csv', 'w') as f:
-                    f.write(beam_)
-                    f.close()
+            filename = 'res' + name + '_' + Arr.name + '_'
+            folder = 'results' + name + '/'
+            if not os.path.isdir(folder):
+                os.mkdir(folder)
+            with open(folder + filename + 'wng.csv', 'w') as f:
+                f.write(wng_)
+                f.close()
+            with open(folder + filename + 'df.csv', 'w') as f:
+                f.write(df_)
+                f.close()
+            with open(folder + filename + 'snr.csv', 'w') as f:
+                f.write(snr_)
+                f.close()
+            with open(folder + filename + 'fnbw.csv', 'w') as f:
+                f.write(fnbw_)
+                f.close()
+            with open(folder + filename + 'beam.csv', 'w') as f:
+                f.write(beam_)
+                f.close()
 
             beam_min = -60
             beam_max = 0
@@ -483,12 +334,8 @@ def simulation(result: str, sizes: Params, name: str):
                 f.write(data_defs)
                 f.close()
 
-        case _:
-            pass
-
 
 def main():
-    result = 'beam'
     sizes = {
         'A': Params(LC=(2, 2, 0),
                     sd=(1, 2, 0),
@@ -525,7 +372,7 @@ def main():
     for name in sizes.keys():
         size = sizes[name]
         print(name)
-        simulation(result, size, name)
+        simulation(size, name)
 
 
 if __name__ == '__main__':
